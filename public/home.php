1eb2fb06d9d0e4852d1938151676c1c1381128bb
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-06-03 10:56:16 -0300

    add- adicionei comentario ao home

--- public/home.php
+++ public/home.php
@@ -1,24 +1,26 @@
 <?php
 session_start();
-if(!isset($_SESSION["usuario"])){
+if(!isset($_SESSION["usuario"])){ // se nao existir uma sessao com esse nome ele vai mandar para o diretorio abaixo
     header("Location: ../index.php");
     exit();
 }
 
 include("../infra/db/connect.php");
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST"){ // vai pegar oque o usuario digitou no campo e passar para uma variavel
     $novoUsuario = $_POST['usuario'];
     $novaSenha = $_POST['senha'];
 
     $sql = "INSERT INTO usuarios (usuario,senha) 
-    VALUES ('$novoUsuario','$novaSenha')";  
+    VALUES ('$novoUsuario','$novaSenha')";   // ele vai inserir o usuario que for digitado para o db
 
     if($conn->query($sql) === TRUE){
         echo "<script> alert('Usuário cadastrado com sucesso!')</script>";
     }else{
         echo "<script> alert('Erro ao cadastrar')</script>";
     }
+
+    // vai verifcar se o usuario e senha estiverem corretos e vai cadastrar
 
 };
 
@@ -31,7 +33,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Home</title>
 </head>
 <body>
-    <h3>Bem-Vindo! <?php echo $_SESSION["usuario"]; ?></h3>
+    <h3>Bem-Vindo! <?php echo $_SESSION["usuario"]; ?> </h3> 
+    <!-- vai mostrar o nome do usuario logado -->
     <a href="logout.php"> Sair</a>
 
     <hr>
@@ -56,7 +59,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <hr>
     <?php
     
-    include("components/table.php")
+    include("components/table.php") 
+    // vai puxar o script do table, tudo que rodar la vai rodar aqui
 
     ?>
 
